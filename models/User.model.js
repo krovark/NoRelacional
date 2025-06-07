956e227eae0bd802ae24a823b9fc585ca34f25cb
Santiagon Gonzalez <santglez51@gmail.com>
2025-06-07 15:59:20 -0300

    adios foto y bio

--- models/User.model.js
+++ models/User.model.js
@@ -5,11 +5,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profilePicture: {
-    type: String,
-    default: 'https://res.cloudinary.com/dstqx4yk0/image/upload/v1747277212/default_profile.png',
-  },
-  bio: { type: String, maxlength: 200, default: '' },
   createdAt: { type: Date, default: Date.now },
 });
 
